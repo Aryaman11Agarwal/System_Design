18f09cc4fd171539b5f135ad3e7d491e10495ed8
Aryaman11Agarwal <aryaman1110@gmail.com>
2025-08-19 23:49:19 +0530

    Added rows and column in postion and made constructor for board class

--- Design_Problems/Tic_Tac_Toe_Game/Tic_Tac_Toe/src/Board.java
+++ Design_Problems/Tic_Tac_Toe_Game/Tic_Tac_Toe/src/Board.java
@@ -1,2 +1,23 @@
 public class Board {
+
+    private Symbol [][] grid;
+    private int rows,columns;
+
+    Board(int rows,int columns){
+        this.rows=rows;
+        this.columns=columns;
+        grid=new Symbol[rows][columns];
+
+        for(int i=0;i<rows;i++){
+            for(int j=0;j<columns;j++)
+                grid[i][j]=Symbol.EMPTY;
+        }
+
+
+    }
+
+    public boolean isValidMove(Position position){
+        return (position.row>=0 && position.row<rows && position.column>=0 &&
+                position.column<columns && grid[position.row][position.column]==Symbol.EMPTY);
+    }
 }
--- Design_Problems/Tic_Tac_Toe_Game/Tic_Tac_Toe/src/Position.java
+++ Design_Problems/Tic_Tac_Toe_Game/Tic_Tac_Toe/src/Position.java
@@ -1,4 +1,4 @@
 public class Position {
 
-
+   public int row,column;
 }
