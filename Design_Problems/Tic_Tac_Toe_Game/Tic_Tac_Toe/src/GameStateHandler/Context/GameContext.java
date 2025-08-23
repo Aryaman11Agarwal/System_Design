4cda52b1d6904bc1211dfa75a598bb83adb223ad
Aryaman11Agarwal <aryaman1110@gmail.com>
2025-08-23 12:22:19 +0530

    GameContext Class completed

--- Design_Problems/Tic_Tac_Toe_Game/Tic_Tac_Toe/src/GameStateHandler/Context/GameContext.java
+++ Design_Problems/Tic_Tac_Toe_Game/Tic_Tac_Toe/src/GameStateHandler/Context/GameContext.java
@@ -1,4 +1,23 @@
 package GameStateHandler.Context;
 
+import GameStateHandler.ConcreteStates.XChanceState;
+import GameStateHandler.GameState;
+import Utils.Player;
+
 public class GameContext {
+
+    private GameState currentState;
+
+    GameContext(){
+        currentState=new XChanceState();
+    }
+
+
+    public void next(Player player,boolean hasWon){
+        currentState.next(this,player,hasWon);
+    }
+
+    public void setCurrentState(GameState gameState){
+        this.currentState=gameState;
+    }
 }
