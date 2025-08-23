f0ac1a6c459100f8e4e878d1dc27679d6094f950
Aryaman11Agarwal <aryaman1110@gmail.com>
2025-08-23 12:55:14 +0530

    Position class completed

--- Design_Problems/Tic_Tac_Toe_Game/Tic_Tac_Toe/src/Utils/Position.java
+++ Design_Problems/Tic_Tac_Toe_Game/Tic_Tac_Toe/src/Utils/Position.java
@@ -1,4 +1,16 @@
 package Utils;
 
 public class Position {
+
+    public int row,col;
+
+    public Position(int row,int col){
+        this.row=row;
+        this.col=col;
+    }
+
+    @Override
+    public String toString(){
+      return row+" , "+col;
+    }
 }
