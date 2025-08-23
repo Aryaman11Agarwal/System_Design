e615a172c09af6a629c8c747b97870c3c017707e
Aryaman11Agarwal <aryaman1110@gmail.com>
2025-08-23 12:34:43 +0530

    Human Player strategy completed

--- Design_Problems/Tic_Tac_Toe_Game/Tic_Tac_Toe/src/PlayerStrategies/ConcreteStrategies/HumanPlayerStrategy.java
+++ Design_Problems/Tic_Tac_Toe_Game/Tic_Tac_Toe/src/PlayerStrategies/ConcreteStrategies/HumanPlayerStrategy.java
@@ -1,6 +1,44 @@
 package PlayerStrategies.ConcreteStrategies;
 
 import PlayerStrategies.PlayerStrategy;
+import Utils.Board;
+import Utils.Position;
+
+import java.util.Scanner;
 
 public class HumanPlayerStrategy implements PlayerStrategy {
+
+
+    private Scanner scanner;
+    private String name;
+
+    HumanPlayerStrategy(String name){
+        this.name=name;
+        scanner=new Scanner(System.in);
+    }
+    @Override
+    public Position makeMove(Board board) {
+
+
+        while(true){
+            System.out.println("Hey "+name+ " Please enter your move ");
+
+            try{
+
+                int row=scanner.nextInt();
+                int col=scanner.nextInt();
+
+                if(board.isValidMove(row,col)){
+                    return new Position(row,col);
+                }
+
+                System.out.println("Invalid move, try again");
+            }
+            catch (Exception e){
+
+                System.out.println("Enter valid integers");
+                scanner.nextLine();
+            }
+        }
+    }
 }
