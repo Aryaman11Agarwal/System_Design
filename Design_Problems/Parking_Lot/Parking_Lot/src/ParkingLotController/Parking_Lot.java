0a8047499e2f29b278a5c1467c6ed1d656842c95
Aryaman11Agarwal <aryaman1110@gmail.com>
2025-09-06 13:28:24 +0530

    Created parking lot controller

--- Design_Problems/Parking_Lot/Parking_Lot/src/ParkingLotController/Parking_Lot.java
+++ Design_Problems/Parking_Lot/Parking_Lot/src/ParkingLotController/Parking_Lot.java
@@ -1,4 +1,33 @@
 package ParkingLotController;
 
+import Utils.ParkingSpot;
+import VehicleController.Vehicle;
+
+import java.util.ArrayList;
+
 public class Parking_Lot {
+
+    private ArrayList<ParkingSpot> parkingSpots;
+
+    void addParkingSpot(ParkingSpot spot){
+        parkingSpots.add(spot);
+    }
+
+    public ParkingSpot getSpot(String vehicleType){
+        for(ParkingSpot parkingSpot:parkingSpots){
+            if(parkingSpot.isVacant() && parkingSpot.getVehicleType().equals(vehicleType)){
+                return parkingSpot;
+            }
+
+
+        }
+
+        return null;
+    }
+
+    public boolean bookSpot(ParkingSpot spot, Vehicle vehicle){
+       return spot.bookSpot(vehicle);
+    }
+
+
 }
