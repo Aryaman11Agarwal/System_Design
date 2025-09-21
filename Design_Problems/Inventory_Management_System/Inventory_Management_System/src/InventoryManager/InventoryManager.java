9ee378227e6bcb1b8714f3bb4f1d6434cce22182
Aryaman11Agarwal <aryaman1110@gmail.com>
2025-09-21 13:30:33 +0530

    updated inventory manager class

--- Design_Problems/Inventory_Management_System/Inventory_Management_System/src/InventoryManager/InventoryManager.java
+++ Design_Problems/Inventory_Management_System/Inventory_Management_System/src/InventoryManager/InventoryManager.java
@@ -1,9 +1,25 @@
 package InventoryManager;
 
+import InventoryObserver.Observer;
+import ProductManager.Product;
+import Warehouse.Warehouse;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
+
 public class InventoryManager {
 
-    private static volatile InventoryManager inventoryManager=null;
-    private InventoryManager(){}
+    private static  InventoryManager inventoryManager=null;
+
+    private ArrayList<Observer> observers;
+    int idptr=0;
+    Map<Integer,Warehouse>  warehouses;
+    private InventoryManager(){
+        warehouses=new HashMap<Integer, Warehouse>();
+        observers=new ArrayList<>();
+    }
 
     public static  InventoryManager getInstance(){
 
@@ -21,6 +37,62 @@ public class InventoryManager {
 
         return inventoryManager;
     }
+
+    public void addWarehouse(Warehouse warehouse){
+
+
+
+         warehouses.put(warehouse.getId(),warehouse);
+
+         System.out.println("New warehouse "+warehouse.getName()+ " created at location "+warehouse.getLocation()+ " with ID:" + warehouse.getId());
+
+
+
+    }
+
+    public void removeWarehouse(int id){
+
+        if(warehouses.containsKey(id)){
+
+
+            warehouses.remove(id);
+
+            System.out.println("Warehouse with id :" + id+ " removed successfully ");
+
+        }
+        else{
+            System.out.println("This ID warehouse does not exist does not exist");
+        }
+    }
+
+    public void addObserver(Observer observer){
+        observers.add(observer);
+    }
+
+    public void notifyObservers(Product product,Warehouse warehouse){
+
+        for(Observer observer:observers){
+            observer.update(product,warehouse);
+        }
+    }
+
+    public void checkAndNotify(){
+
+       for(Integer key: warehouses.keySet()){
+           Warehouse warehouse=warehouses.get(key);
+           Map<String, Product> products=warehouse.getProducts();
+
+
+           for(String productKey:products.keySet()){
+
+               notifyObservers(products.get(productKey),warehouse);
+           }
+       }
+    }
+
+
+
+
 
 
 
